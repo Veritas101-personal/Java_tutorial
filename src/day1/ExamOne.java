c0d014079b472a3be7a20182b4cb54ec7dd13867
Juyoung Kim <kjyoung153@hotmail.com>
2020-04-17 16:10:14 +0900

    First exam

---
+++ src/day1/ExamOne.java
@@ -0,0 +1,22 @@
+package day1;
+import java.util.Scanner;
+
+public class ExamOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("값을 입력하세요.");
+		
+		int a = scan.nextInt();
+		System.out.println(a);
+		
+		scan.close();
+		
+		
+		
+	}
+
+}
