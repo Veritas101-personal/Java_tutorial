ac811346fa29c7693d091b3259823cfe1d867b6b
Juyoung Kim <kjyoung153@hotmail.com>
2020-04-23 12:17:58 +0900

    d

--- src/day5/MethodEx2.java
+++ src/day5/MethodEx2.java
@@ -2,4 +2,19 @@ package day5;
 
 public class MethodEx2 {
 
+	public static void main(String[] args) {
+		// 생성자 : 멤버변수 초기화
+		// 기본 생성자
+		// public 클래스명() {
+		// 
+		// }
+		// 이름이 같은 메소드나 생성자는 존재할 수 있다. 물론 다음과 같은 조건을 만족해야 하지만.
+		// - 매개변수가 달라지는 경우
+		// 1) 매개변수의 갯수가 달라지는 경우
+		// 2) 매개변수의 타입이 달라지는 경우 int, double 등등...
+		// 이런 걸 오버로딩이라고 한다! @overload
+		
+		
+	}
+	
 }
