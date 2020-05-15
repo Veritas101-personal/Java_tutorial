22bb95752c7b551873039e23df370727b961724c
Juyoung Kim <kjyoung153@hotmail.com>
2020-05-15 09:31:06 +0900

    Problem solved!

--- src/day14/BaseballTest_List.java
+++ src/day14/BaseballTest_List.java
@@ -7,8 +7,9 @@ public class BaseballTest_List {
 	static ArrayList<Integer> pitching = new ArrayList<Integer>();
 	static ArrayList<Integer> hit = new ArrayList<Integer>();
 	
-	static int strike = 0;
+	static int strike = 0; 
 	
+	// 여기까진 모두 클래스 변수들 
 
 	public static void main(String[] args) {
 
@@ -16,11 +17,11 @@ public class BaseballTest_List {
 		
 		System.out.println("플레이볼!");
 		
-		hitter(scan);
-		pitcher();
-		detect();
+		
+		pitcher(); // 먼저 공부터 던져야지
+		
 
-		while (strike != 3) {
+		while (strike != 3) { // 스트라이크 3될때 까지 못지나간다
 			
 			hitter(scan);
 			detect();
@@ -46,8 +47,8 @@ public class BaseballTest_List {
 			} else {
 				
 			}
-			
 		}
+		//System.out.println(pitching);  확인용!
 	}
 	
 	public static void hitter (Scanner scan) {
@@ -55,7 +56,7 @@ public class BaseballTest_List {
 		int min = 1, max = 9;
 		int inning = 3;
 		int cnt=0;
-		hit.clear();
+		hit.clear(); // 이거 안하면 니가 했던거 초기화 안되서 그대로 남는다...
 		
 		System.out.println("1~9까지 번호를 입력하세요.");
 		while (cnt<inning) {
@@ -78,27 +79,21 @@ public class BaseballTest_List {
 	public static void detect () {
 		
 		int ball = 0;
-		int cnt = 0;
-	
+		strike = 0; // 이거 안하면 스트라이크, 볼 초기화 안된다...
 		
-			while (cnt<9 || ball < 3) {
-				
 				for (int i = 0 ; i < 9 ; i ++) {
-					strike = 0;
-					if (pitching.indexOf(i)==hit.indexOf(i)) {
-						strike++;
-						
-					} else if (pitching.contains(hit.get(i))) {
-						ball++;
-				
-					} else {
+					if (pitching.contains(i) && hit.contains(i)) {
+						if (pitching.indexOf(i)==hit.indexOf(i)) {
+							strike++;
+						} else if (pitching.contains(i) && hit.contains(i)) {
+							ball++;
+							 	
+						}
 						
 					}
+					
 				}
-				cnt++;
-			}
 			
-
 		System.out.println("Strike : " + strike + " / Ball : " +ball);
 
 			
